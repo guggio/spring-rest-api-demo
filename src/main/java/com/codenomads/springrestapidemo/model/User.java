93fb081a72e0753b0dccf50e72545b484acfd5e9
sebastianguggisberg <sebastian.guggisberg@gmail.com>
2023-02-12 13:56:35 +0100

    added unique email field to the User and removed the id from the dto. changed the id parameters in the apis to email, so that the id would not be needed to be exposed to the outside. Additionally, changed the type of the birthDate field to LocalDate to cope with timezone issues.

--- src/main/java/com/codenomads/springrestapidemo/model/User.java
+++ src/main/java/com/codenomads/springrestapidemo/model/User.java
@@ -2,10 +2,13 @@ package com.codenomads.springrestapidemo.model;
 
 
 import lombok.*;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Size;
-import java.util.Date;
+import java.time.LocalDate;
 import java.util.UUID;
 
 @Entity
@@ -23,7 +26,11 @@ public class User {
 
     @Size(min = 3, max = 30)
     private String name;
-    @Temporal(TemporalType.DATE)
-    private Date birthDate;
+    @NotEmpty
+    @Email(regexp = "^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$")
+    @Column(unique = true)
+    private String email;
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
+    private LocalDate birthDate;
 
 }
